585f2bdd9db76b8097e962152ad53b008159003c
satmix2000 <satmix2000@yahoo.com>
2026-08-09 21:16:06 -0300

    Add preconnect links for Google Fonts
    Added preconnect links for Google Fonts in the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </head>
       <body>{children}</body>
     </html>
   )
